ba3f33983942d1aeb9f6c9d6e9b37b5d3baf77e0
JulianDolang <chrisngantung3@gmail.com>
2024-04-30 16:27:40 +0700

    update login overlay

---
+++ script.js
@@ -0,0 +1,9 @@
+function closeForm() {
+  const elm = document.getElementById("modal");     
+  elm.style.display="none"; 
+}  
+
+function showForm() {     
+  const elm = document.getElementById("modal");      
+  elm.style.display="flex"; 
+}
